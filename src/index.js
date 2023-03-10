48321e02e2c76403e37befa233dfe6fd47668e28
taeryu7 <pegasy02@gmail.com>
2023-03-10 09:41:15 +0900

    0310_before_1

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
 
-const store = createStore(rootReducer); // 스토어를 만든다.
+const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만든다.
+// composeWithDevTools 를 사용하여 리덕스 개발자 도구 활성화
 
 ReactDOM.render(
   <Provider store={store}>
@@ -15,5 +17,6 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
 
 serviceWorker.unregister();
