eec2c37a65fa2162f2e4a1fb84a50861b9337fd3
taeryu7 <pegasy02@gmail.com>
2023-03-10 10:28:17 +0900

    0310_before_3

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,11 +1,73 @@
-
-
 // connect 사용하기
+// connect 함수 더 깔끔하게 작성하기
+// mapStateToProps와 mapDispatchToProps를 따로 선언하지 않고 connect 함수를 사용 할 때 인자 쪽에서 익명함수로 바로 만들어서 사용하면 코드가 더 깔끔해진다.
+import React, { useCallback } from 'react';
+import { connect } from 'react-redux';
+import Todos from '../components/Todos';
+import { addTodo, toggleTodo } from '../modules/todos';
+
+function TodosContainer({ todos, addTodo, toggleTodo }) {
+  const onCreate = text => addTodo(text);
+  const onToggle = useCallback(id => toggleTodo(id), [toggleTodo]); // 최적화를 위해 useCallback 사용
+
+  return <Todos todos={todos} onCreate={onCreate} onToggle={onToggle} />;
+}
+
+export default connect(
+  state => ({ todos: state.todos }),
+  {
+    addTodo,
+    toggleTodo
+  }
+)(TodosContainer);
 
 
+/*
 // connect 함수에서는 mapDispatchToProps가 함수가 아니라 아예 객체형태일때에는 bindActionCreators를 대신 호출해준다.
+import React from 'react';
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+import { increase, decrease, setDiff } from '../modules/counter';
 
+function CounterContainer({ number, diff, increase, decrease, setDiff }) {
+  return (
+    <Counter
+      // 상태와
+      number={number}
+      diff={diff}
+      // 액션을 디스패치 하는 함수들을 props로 넣어준다.
+      onIncrease={increase}
+      onDecrease={decrease}
+      onSetDiff={setDiff}
+    />
+  );
+}
 
+// mapStateToProps 는 리덕스 스토어의 상태를 조회해서 어떤 것들을 props 로 넣어줄지 정의한다.
+// 현재 리덕스 상태를 파라미터로 받아온다.
+const mapStateToProps = state => ({
+  number: state.counter.number,
+  diff: state.counter.diff
+});
+
+// mapDispatchToProps가 함수가 아니라 객체면
+// bindActionCreators 를 connect 에서 대신 해준다.
+const mapDispatchToProps = {
+  increase,
+  decrease,
+  setDiff
+};
+
+// connect 함수에는 mapStateToProps, mapDispatchToProps 를 인자로 넣어야한다.
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CounterContainer);
+
+// 위 코드는 다음과 동일하다.
+//  const enhance = connect(mapStateToProps, mapDispatchToProps);
+//  export defualt enhance(CounterContainer);
+*/
 
 /*
 // redux 라이브러리에 내장된 bindActionCreators 함수 사용 코드
